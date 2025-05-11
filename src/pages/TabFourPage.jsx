bc1939691b842aa76e63ae3be8d01fa810ea08fb
Mandar Pandya <mandarpandya54@gmail.com>
2025-05-11 16:32:01 +0530

    feat: add todo functionality

--- src/pages/TabFourPage.jsx
+++ src/pages/TabFourPage.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import {
+  Box,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../redux/todoSlice";
 
 const TabFourPage = () => {
-  return <div>TabFourPage</div>;
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todo.todos);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = () => {
+    if (inputValue.trim()) {
+      dispatch(addTodo({ id: Date.now(), title: inputValue }));
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    console.log(id);
+  };
+  return (
+    <Box>
+      <Box display="flex" gap={2} mb={2}>
+        <TextField
+          label="Add a new task"
+          variant="outlined"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyPress={(e) => e.key === "Enter" && handleAddTodo()}
+          fullWidth
+        />
+        <Button variant="contained" color="primary" onClick={handleAddTodo}>
+          Add
+        </Button>
+      </Box>
+      {todos.length > 0 && (
+        <List sx={{ maxHeight: "400px", overflow: "auto" }}>
+          {todos.map((todo, index) => (
+            <ListItem
+              key={index}
+              secondaryAction={
+                <Button
+                  edge="end"
+                  onClick={() => handleDeleteTodo(todo?.id)}
+                  aria-label="delete"
+                >
+                  delete
+                </Button>
+              }
+            >
+              <ListItemText primary={todo?.title} />
+            </ListItem>
+          ))}
+        </List>
+      )}
+
+      {todos.length === 0 && (
+        <Typography
+          variant="body1"
+          color="text.secondary"
+          textAlign="center"
+          mt={2}
+        >
+          No tasks yet. Add one above!
+        </Typography>
+      )}
+    </Box>
+  );
 };
 
 export default TabFourPage;
---
+++ src/redux/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
