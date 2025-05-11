b0d0118d9791558e0d67cbf6da1c7fba9b76b481
Mandar Pandya <mandarpandya54@gmail.com>
2025-05-11 14:52:38 +0530

    feat: added navigation button in tab two

--- src/pages/TabTwoPage.jsx
+++ src/pages/TabTwoPage.jsx
@@ -1,7 +1,21 @@
+import { Box, Button } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const TabTwoPage = () => {
-  return <div>TabTwoPage</div>;
+  const navigate = useNavigate();
+  return (
+    <Box>
+      <Button
+        onClick={() => navigate("/tab3")}
+        variant="contained"
+        color="primary"
+        sx={{ mb: 2 }}
+      >
+        Navigate to Tab 3
+      </Button>
+    </Box>
+  );
 };
 
 export default TabTwoPage;
