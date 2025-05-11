5b6041837546778072966e2de0b1737e9556cc29
Mandar Pandya <mandarpandya54@gmail.com>
2025-05-11 15:57:22 +0530

    feat: Tab Five task completed

--- src/pages/TabFivePage.jsx
+++ src/pages/TabFivePage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Button, Stack } from "@mui/material";
+import React, { useState } from "react";
 
 const TabFivePage = () => {
-  return <div>TabFivePage</div>;
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  return (
+    <Stack
+      spacing={{ xs: 1, sm: 2 }}
+      direction="row"
+      useFlexGap
+      sx={{ flexWrap: "wrap" }}
+    >
+      <Button
+        type="button"
+        variant="contained"
+        color="primary"
+        onClick={() => setCount1(count1 + 1)}
+      >
+        {count1}
+      </Button>
+      <Button
+        type="button"
+        variant="contained"
+        color="primary"
+        onClick={() => setCount2(count2 + 1)}
+      >
+        {count2}
+      </Button>
+    </Stack>
+  );
 };
 
 export default TabFivePage;
