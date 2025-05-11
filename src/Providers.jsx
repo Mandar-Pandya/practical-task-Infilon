26970661700602b3e8e3ace44dfbcc018730cb70
Mandar Pandya <mandarpandya54@gmail.com>
2025-05-11 17:35:57 +0530

    feat: added hashrouter

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
@@ -7,11 +7,11 @@ import { Provider } from "react-redux";
 const queryClient = new QueryClient();
 const Providers = ({ children }) => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>{children}</Provider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
